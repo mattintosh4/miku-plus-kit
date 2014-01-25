365a0a9ded6f616f8db06913fd939310f4e5a2ca
mattintosh4 <mattintosh4@gmail.com>
2014-01-26 04:21:36 +0900

    no message

--- distfile.sh
+++ distfile.sh
@@ -1,101 +1,78 @@
-#!/bin/sh
-PS4=
-define(){ eval ${1:?}=\"\${*\:2}\"; }
-
+#!/bin/ksh
 set -e
 set -u
+PS4=
 set -x
 
-define  PROJECT_NAME        MikuPlusKit
-define  PROJECT_REALNAME    MikuInstaller+ Kit
-define  PROJECT_ROOT        $(cd "$(dirname "$0")"; pwd)
+# Command definition
+CAT=/bin/cat
+HDIUTIL=/usr/bin/hdiutil
+HDIUTIL_CREATE="$HDIUTIL create -ov -format UDBZ"
+INSTALL=/usr/bin/install
+LN=/bin/ln
+MKDIR="/bin/mkdir -p"
+MKTEMP=/usr/bin/mktemp
+PERL=/usr/bin/perl
+RM=/bin/rm
+RSYNC="/usr/bin/rsync -a"
+SED=/usr/bin/sed
+TR=/usr/bin/tr
 
-define  DITTO               /usr/bin/ditto
-define  GIT                 /opt/local/bin/git
-define  HDIUTIL             /usr/bin/hdiutil
-define  INSTALL             /usr/bin/install
-define  LN                  /bin/ln
-define  MKTEMP              /usr/bin/mktemp
-define  SED                 /usr/bin/sed
-define  PERL                /usr/bin/perl
-define  PERL_MD_MODULE      $PROJECT_ROOT/Markdown.pl
+# External command definition
+GIT=/usr/local/git/bin/git
 
-define  PROJECT_VERSION     $(GIT_DIR=$PROJECT_ROOT/.git $GIT log -1 --pretty --date=short --format=%ad) ; : ${PROJECT_VERSION}
-define  PROJECT_HASH        $(GIT_DIR=$PROJECT_ROOT/.git $GIT log -1 --pretty --format=%h)               ; : ${PROJECT_HASH}
-define  PROJECT_URL         https://github.com/mattintosh4/miku-plus-kit
 
-define  BUNDLE_SOURCE       $1
-define  BUNDLE_NAME         Wine.bundle
-define  BUNDLE_VERSION      $($BUNDLE_SOURCE/bin/wine --version)
+# Variable definition
+_WINE_SOURCE_DIR=$1
+_WINE_VERSION=wine-1.7.9
 
-define  DISTFILE_VOLN       ${PROJECT_NAME}_${PROJECT_VERSION}
-define  DISTFILE_NAME       ${PROJECT_NAME}_${PROJECT_VERSION}_${PROJECT_HASH}.dmg
-define  DISTFILE_PATH       $PROJECT_ROOT/$DISTFILE_NAME
+_PROJECT_SOURCE_DIR=$(cd "$(dirname "$0")" && pwd)
+_PROJECT_NAME=MPK
+_PROJECT_VERSION=$(GIT_DIR=$_PROJECT_SOURCE_DIR/.git $GIT log -1 --pretty --date=short --format=%ad)
 
-#-------------------------------------------------------------------------------
-
-make_readme()
-{
-    {
-        cat <<EOS
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8" />
-<link rel="stylesheet" href=".rsrc/style.css" />
-<title>$PROJECT_REALNAME $PROJECT_VERSION</title>
-</head>
-<body>
-EOS
-        $PERL $PERL_MD_MODULE $PROJECT_ROOT/README.md
-        cat <<EOS
-</body>
-</html>
-EOS
-    } | tr -d "\n" > $tmpdir/README.html
-
-    $INSTALL -m 0644 $PROJECT_ROOT/README.css $tmpdir_rsrc/style.css
-}
-
-main()
-{
-    tmpdir=$($MKTEMP -d -t $$$$)
-    tmpdir_rsrc=$tmpdir/.rsrc
-    trap "rm -rf $tmpdir" SIGINT EXIT
-
-    $DITTO $BUNDLE_SOURCE $tmpdir_rsrc/$BUNDLE_NAME/Contents/SharedSupport
-    $LN -s SharedSupport  $tmpdir_rsrc/$BUNDLE_NAME/Contents/SharedFrameworks
-
-    $INSTALL -m 0755 $PROJECT_ROOT/update.sh.in  $tmpdir/update.sh
-    $INSTALL -m 0755 $PROJECT_ROOT/main.sh.in    $tmpdir_rsrc/main.sh
-    $INSTALL -m 0644 $PROJECT_ROOT/patch.diff.in $tmpdir_rsrc/patch.diff
-
-    make_readme
-
-    items=(
-        $tmpdir_rsrc/main.sh
-        $tmpdir_rsrc/patch.diff
-        $tmpdir/README.html
-        $tmpdir/update.sh
-    )
-    $SED -i "" "
-        s|@VERSION@|$PROJECT_VERSION|g
-        s|@WINE_VERSION@|$BUNDLE_VERSION|g
-        s|@DISTFILE_NAME@|$DISTFILE_NAME|g
-        s|@GitHub@|$PROJECT_URL|g
-    " "${items[@]}"
-
-    $HDIUTIL create -ov -attach \
-        -format  UDBZ \
-        -srcdir  $tmpdir \
-        -volname $DISTFILE_VOLN \
-                 $DISTFILE_PATH
-}
+_DISTFILE_NAME=$_PROJECT_NAME\_$_PROJECT_VERSION
+_DISTFILE_VOLUME_NAME=$_PROJECT_NAME
 
 #-------------------------------------------------------------------------------
 
-case ${BUNDLE_VERSION:?} in
-wine-1.*)
-    main
-    ;;
-esac
+tempdir=$($MKTEMP -d -t $$)
+trap "$RM -r $tempdir" INT EXIT
+cd $tempdir
+
+set -- .rsrc/MikuInstaller.app/Contents/Resources/Wine.bundle/Contents/Resources
+$MKDIR "$1"
+$RSYNC -R --filter='- .DS_Store' "$_WINE_SOURCE_DIR"/./ "$1"/SharedSupport
+$LN -s SharedSupport "$1"/SharedFrameworks
+
+
+$INSTALL -m 0755 "$_PROJECT_SOURCE_DIR"/update.sh        update.sh
+$INSTALL -m 0644 "$_PROJECT_SOURCE_DIR"/patch.diff       .rsrc/patch.diff
+$INSTALL -m 0644 "$_PROJECT_SOURCE_DIR"/patch-plist.diff .rsrc/patch-plist.diff
+$INSTALL -m 0644 "$_PROJECT_SOURCE_DIR"/README.css       .rsrc/README.css
+
+{
+$CAT <<!
+<!doctype html><html><head><meta charset="utf-8" /><link rel="stylesheet" href=".rsrc/README.css" /><title>$_PROJECT_NAME $_PROJECT_VERSION</title></head><body>
+!
+$PERL "$_PROJECT_SOURCE_DIR"/Markdown.pl \
+      "$_PROJECT_SOURCE_DIR"/README.md
+$CAT <<!
+</body></html>
+!
+} \
+| $TR -d '\n' \
+> README.html
+
+
+set -- update.sh \
+       README.html \
+       .rsrc/patch.diff \
+       .rsrc/patch-plist.diff
+$SED -i '' -f /dev/fd/0 "$@" <<!
+s/@VERSION@/$_PROJECT_VERSION/g
+s/@WINE_VERSION@/$_WINE_VERSION/g
+s/@DISTFILE_NAME@/$_DISTFILE_NAME/g
+s/@DISTFILE_VOLUME_NAME@/$_DISTFILE_VOLUME_NAME/g
+!
+
+$HDIUTIL_CREATE -srcdir . -volname "$_DISTFILE_VOLUME_NAME" "$_PROJECT_SOURCE_DIR"/"$_DISTFILE_NAME"
---
+++ update.sh
@@ -0,0 +1,72 @@
+#!/bin/ksh
+set -e
+set -u
+PS4=
+set -x
+
+# Command definition
+HDIUTIL=/usr/bin/hdiutil
+HDIUTIL_ATTACH="$HDIUTIL attach -quiet -nobrowse"
+HDIUTIL_CREATE="$HDIUTIL create -ov -format UDBZ"
+HDIUTIL_DETACH="$HDIUTIL detach -quiet"
+MD5=/sbin/md5
+MKTEMP=/usr/bin/mktemp
+OPEN=/usr/bin/open
+PATCH=/usr/bin/patch
+RSYNC="/usr/bin/rsync -a"
+
+# Variable definition
+_MIKU_DMG=$1
+_MIKU_DMG_VOLUME_NAME=MikuInstaller
+_MIKU_APP_NAME=MikuInstaller.app
+_MIKU_WINE_BUNDLE_PREFIX=$_MIKU_APP_NAME/Contents/Resources/Wine.bundle
+_MIKU_WINE_BUNDLE_PLIST=$_MIKU_WINE_BUNDLE_PREFIX/Contents/Info.plist
+
+_MY_NAME=MikuInstaller+
+_MY_VERSION=@VERSION@
+_MY_WINE_VERSION=@WINE_VERSION@
+_MY_PREFIX=$(cd "$(dirname "$0")" && pwd)
+_MY_DATA_DIR=$_MY_PREFIX/.rsrc
+_MY_NEW_DMG_FILE_NAME=$_MY_NAME\_$_MY_VERSION.dmg
+_MY_NEW_DMG_VOLUME_NAME=$_MY_NAME
+
+#-------------------------------------------------------------------------------
+
+# Check MikuInstaller disk image
+set -- $($MD5 -q "$_MIKU_DMG")
+case $1 in
+36b33041fea516faccddc18413ea23cc)
+    ;;
+*)
+    echo "Invalid disk image: $_MIKU_DMG"
+    exit 1
+    ;;
+esac
+
+
+# Import MikuInstaller resources
+set -- $($MKTEMP -u -t $$)
+$HDIUTIL_ATTACH "$_MIKU_DMG" -mountpoint "$1"
+$RSYNC -R --filter='. -' "$1"/./"$_MIKU_APP_NAME" . <<!
+- SharedFrameworks/
+- SharedSupport/
+- addapp.sh
+- createapp.sh
+- infoplist.awk
+- startx11.sh
+- shellexec.exe.so
+- startmenu.exe.so
+!
+$HDIUTIL_DETACH "$1"
+
+
+# Update MikuInstaller resources
+$RSYNC -R "$_MY_DATA_DIR"/./"$_MIKU_APP_NAME" .
+$PATCH -Nsp1 < "$_MY_DATA_DIR"/patch.diff
+$PATCH -Nsp1 < "$_MY_DATA_DIR"/patch-plist.diff
+
+
+# Create new disk image
+$HDIUTIL_CREATE "$_MY_NEW_DMG_FILE_NAME" \
+    -srcdir  "$_MIKU_APP_NAME" \
+    -volname "$_MY_NEW_DMG_VOLUME_NAME"
